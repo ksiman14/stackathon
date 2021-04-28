7ec88e311a673b856cfabcd0e2dfa6db994bd8a0
Kelsey Siman <ksiman14@gmail.com>
2021-04-28 18:15:40 -0400

    refactor: removed all refs to THREE

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import * as THREE from 'three';
 
-/**
- * COMPONENT
- */
 export class Home extends Component {
   componentDidMount() {
     //
--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Line from './components/Line';
 
 /**
  * COMPONENT
